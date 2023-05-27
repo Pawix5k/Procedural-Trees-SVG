edb8801d4f5d2d2a91d651ea6aaf05b131ce363c
guarando <pawelpurzyk@gmail.com>
2023-05-27 11:14:59 +0200

    Move to Line object for drawing

--- main.py
+++ main.py
@@ -13,13 +13,13 @@ tree_params_dict = {
     "offshoot_chance": 0.6,
     "delta_offshoot_chance": 0.8,
     "angles_of_offshoot": (-15., 15.),
-    "epsilon_angles": 0.2,
-    "trunk_length": 50.,
+    "epsilon_angles": 0.4,
+    "trunk_length": 300.,
     "delta_trunk_length": 0.87,
     "epsilon_trunk_length": 0.2,
     "initial_angle": 0.,
     "stop_no_growth_until": 3,
-    "trunk_width": 7,
+    "trunk_width": 50,
     "delta_trunk_width": 0.8,
 }
 tree_params = TreeParams(**tree_params_dict)
--- svg.py
+++ svg.py
@@ -1,6 +1,9 @@
 import math
+from collections import namedtuple
 
 from trunk import Trunk
+
+Line = namedtuple("Line", ["start_x", "start_y", "end_x", "end_y", "width", "angle"])
 
 
 def get_end_cords(length: float, angle: int, start_x: int, start_y: int) -> tuple[int, int]:
@@ -9,15 +12,22 @@ def get_end_cords(length: float, angle: int, start_x: int, start_y: int) -> tupl
     return end_x, end_y
 
 
-def get_line_string(start_x: int, start_y: int, end_x: int, end_y: int, width: float):
-    return f'<line x1="{start_x}" y1="{start_y}" x2="{end_x}" y2="{end_y}" stroke="black" stroke-width="{width}"/>'
+def get_line_string(line: Line):
+    a = get_end_cords(line.width / 2, line.angle - 90, line.start_x, line.start_y)
+    b = get_end_cords(line.width / 2, line.angle + 90, line.start_x, line.start_y)
+    c = get_end_cords(line.width / 2, line.angle + 90, line.end_x, line.end_y)
+    d = get_end_cords(line.width / 2, line.angle - 90, line.end_x, line.end_y)
+
+    polygon = f'''<polygon points="{a[0]},{a[1]} {b[0]},{b[1]} {c[0]},{c[1]} {d[0]},{d[1]}"
+    fill="#713C03" stroke="black" stroke-width="3"/>'''
+    return polygon
 
 
 def generate_lines(trunk: Trunk, start_x: int, start_y: int) -> None:
     lines = []
     def recursively_add_line(trunk: Trunk, start_x: int, start_y: int):
         end_x, end_y = get_end_cords(trunk.length, trunk.angle, start_x, start_y)
-        lines.append([start_x, start_y, end_x, end_y, trunk.width])
+        lines.append(Line(start_x, start_y, end_x, end_y, trunk.width, trunk.angle))
         for child in trunk.children:
             recursively_add_line(child, end_x, end_y)
 
@@ -25,13 +35,16 @@ def generate_lines(trunk: Trunk, start_x: int, start_y: int) -> None:
     return lines
 
 
-def get_bounding_box(lines: list[int]) -> tuple[int]:
-    x_min, y_min, x_max, y_max, _ = lines[0]
+def get_bounding_box(lines: list[Line]) -> tuple[int]:
+    x_min = min(lines[0].start_x, lines[0].end_x)
+    x_max = max(lines[0].start_x, lines[0].end_x)
+    y_min = min(lines[0].start_y, lines[0].end_y)
+    y_max = max(lines[0].start_y, lines[0].end_y)
     for line in lines:
-        for x_cord in line[::2]:
+        for x_cord in (line.start_x, line.end_x):
             x_min = min(x_min, x_cord)
             x_max = max(x_max, x_cord)
-        for y_cord in line[1::2]:
+        for y_cord in (line.start_y, line.end_y):
             y_min = min(y_min, y_cord)
             y_max = max(y_max, y_cord)
     
@@ -50,15 +63,15 @@ def generate_svg_from_tree(trunk: Trunk) -> None:
     bounding_box = get_bounding_box(lines)
     x_min, y_min, x_max, y_max = bounding_box
     vp = (
-        x_min - 50,
-        y_min - 50,
-        x_max - x_min + 100,
-        y_max - y_min + 100
+        x_min - 200,
+        y_min - 200,
+        x_max - x_min + 400,
+        y_max - y_min + 400
     )
     svg_content = []
     svg_content.append(f'<svg viewBox="{vp[0]} {vp[1]} {vp[2]} {vp[3]}" xmlns="http://www.w3.org/2000/svg">')
     for line in lines:
-        svg_content.append(get_line_string(*line))
+        svg_content.append(get_line_string(line))
     svg_content.append("</svg>")
     svg_content = "\n".join(svg_content)
     save_svg(svg_content)
